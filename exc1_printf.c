ed726ea1edca741de3307d2ef81bb72af649215f
Hilmi Ege Kara <116761054+yedksapka@users.noreply.github.com>
2022-10-28 13:12:10 +0300

    Update exc1_printf.c

--- exc1_printf.c
+++ exc1_printf.c
@@ -4,25 +4,3 @@ int main()
     printf("Hello World\n");
     return 0;
 }
-
-//single line
-
-/*
-multiple line
-1
-2
-*/
-
-//<stdio.h> input ya da output varsa kullanılır
-
-/*
-
-
-\n newline 
-\horizontal tab
-\a alert.sound the system bell
-\\ backslash insert a backslash character in a string 
-\" double quote.insert a double quote character in a string 
-
-
-*/
